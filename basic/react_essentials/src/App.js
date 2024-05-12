2aa4487de13e55867111464f5add983a617b4ee6
Dhiraj <dhirajgede@gmail.com>
2024-05-12 18:49:48 +0530

    Added react fragment to remove unneccessary div

--- basic/react_essentials/src/App.js
+++ basic/react_essentials/src/App.js
@@ -3,10 +3,10 @@ import Header from './components/Header';
 import Main from './components/Main';
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Main />
-    </div>
+    </>
   );
 }
 
